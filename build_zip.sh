ced5116f7bc2b2cc64c31b3025d4cb20174c161c
Mike Lothian <mike@fireburn.co.uk>
2025-06-20 15:46:00 +0100

    Rewrite as a Chrome v3 Plugin using PBKDF2 with SHA-256
    Uses the secure, modern algorithm PBKDF2 with SHA-256, accessed via the native Web Crypto API.

--- build_zip.sh
+++ build_zip.sh
@@ -2,27 +2,11 @@
 
 cd "$(dirname "$0")"
 
-. settings.conf
-[ ! -e "./buildnum" ] && echo 0 > ./buildnum
-
-BUILDNUM=$(echo $(cat ./buildnum))
+mkdir -p dist/
 
 cd src/
 
-cat manifest.json \
- | sed '2 s/.*/  "name": "'"$APPNAME"'",/' \
- | sed '3 s/.*/  "version": "0.4.3.'$BUILDNUM'",/' \
-> .manifest.json
-mv .manifest.json manifest.json
-
-ZIPFILE="../dist/chrome-pwdhash-$(git rev-parse --abbrev-ref HEAD).zip"
+ZIPFILE="../dist/chrome-pwdhash.zip"
 [ -e "$ZIPFILE" ] && rm "$ZIPFILE"
-zip -q -r "$ZIPFILE" .
-
-git checkout HEAD manifest.json
-
-cd ../
-
-(( BUILDNUM++ ))
-echo $BUILDNUM > ./buildnum
+zip -q -r -9 "$ZIPFILE" .
 
